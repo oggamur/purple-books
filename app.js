1468d43d1968105f0dae6710a9e11f5d2c3e3c90
David <gidlos55@gmail.com>
2025-11-20 01:51:35 +0300

    ex/ Выполнение задания из 1 модуля

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const users = [
+  { id: 1, name: 'Вася' },
+  { id: 1, name: 'Вася' },
+  { id: 2, name: 'Петя' },
+  { id: 1, name: 'Вася' },
+]
 
+const uniqIds = new Set()
+const usersUniq = []
+
+users.map(({ id }) => {
+  if (!uniqIds.has(id)) {
+    uniqIds.add(id)
+
+    usersUniq.push(users.find((user) => user.id === id))
+  }
+})
+
+console.log(usersUniq)
