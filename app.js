56be85ab5cbc23425329a7989f646afec0ed15c6
David <gidlos55@gmail.com>
2025-11-26 20:08:18 +0300

    ex/ Выполнение задания 3-birthday

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const throwDice = (dice) => {
-  let diceType = Number(dice.split('').slice(1))
-  return Math.floor(Math.random() * (diceType - 1 + 1) + 1)
+const isElderFourteen = (date) => {
+    const regExp = /\d\d\d\d-\d\d-\d\d/;
+
+    if (!regExp.test(date)){
+      return 'Введите день рождения в формате ГГГГ-ММ-ДД';
+    }
+
+    const birthDate = new Date(date);
+    const now = new Date();
+
+    return now.getFullYear() - birthDate.getFullYear() < 14 ? false : true
+      
 }
 
-console.log(throwDice('d6'))
+console.log(isElderFourteen('2000-01-01'));
+console.log(isElderFourteen('2012-01-01'));
