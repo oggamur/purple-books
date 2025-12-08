8abb5eb9d5d484d2b73ec9c787b8ebffc2fc3f57
David <gidlos55@gmail.com>
2025-12-09 00:06:57 +0300

    ex / Поправки v.1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function setTimer () {
     const date = new Date();
     const newYear = new Date(date.getFullYear() + 1, 0, 1);
     const months = Math.floor((newYear - date) / 1000 / 60 / 60 / 24 / 30);
-    const days = Math.floor((newYear - date) / 1000 / 60 / 60 / 24);
+    const days = Math.floor((newYear - date) / 1000 / 60 / 60 / 24) - months * 30;
     const hours = Math.floor((newYear - date) / 1000 / 60 / 60) % 24;
     const minutes = Math.floor((newYear - date) / 1000 / 60) % 60;
     const seconds = Math.floor((newYear - date) / 1000) % 60;
